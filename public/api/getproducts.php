1e06f876663bee4e08cab01fb93517f666a1ab83
Jason <jasonyata@gmail.com>
2019-03-29 16:50:58 -0700

    added basic product list endpoint wrote query for product list added join for images pushed images into array fixed price to number converted data to json

--- public/api/getproducts.php
+++ public/api/getproducts.php
@@ -2,4 +2,41 @@
 
 require_once('mysqlconnect.php');
 
+$query = 'SELECT p.id, p.name, p.price,
+        i.url AS `images`
+        FROM `products` AS p JOIN `images` AS i ON p.`id` = i.`products_id`
+        ORDER BY p.`id`';
+
+$result = mysqli_query($conn, $query);
+
+$images = [];
+$data = [];
+while($row = mysqli_fetch_assoc($result)){
+    $currentID = $row['id'];
+    if ( isset($data[$currentID]) ){
+        $image = $row['images'];
+        $data[$currentID]['images'][] = $image;
+    } else {
+        $image = $row['images'];
+        unset($row['images']);
+        $row['images'] = [];
+        $row['images'][] = $image;
+        $row['price'] = intval($row['price']);
+        $data[$currentID] = $row;
+    }
+};
+
+$pureData = [];
+foreach($data as $value){
+    $pureData[] = $value;
+};
+
+$output = [
+    'success' => true,
+    'products' => $pureData
+];
+
+$json_output = json_encode($output);
+
+print($json_output);
 ?>
