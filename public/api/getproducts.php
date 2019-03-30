77a422496f120056ffc02f98db7f4cf322295043
Jason <jasonyata@gmail.com>
2019-03-29 18:07:09 -0700

    finalized product list added error handling to endpoint

--- public/api/getproducts.php
+++ public/api/getproducts.php
@@ -1,6 +1,11 @@
 <?php
 
+//like putting script tags at top of html doc
 require_once('mysqlconnect.php');
+require_once('functions.php');
+
+//function in parameter must be in string
+set_exception_handler('handleError');
 
 $query = 'SELECT p.id, p.name, p.price,
         i.url AS `images`
@@ -8,6 +13,10 @@ $query = 'SELECT p.id, p.name, p.price,
         ORDER BY p.`id`';
 
 $result = mysqli_query($conn, $query);
+
+if (!$result){
+    throw new Exception('invalid query: '. mysqli_error($conn));
+}
 
 $images = [];
 $data = [];
