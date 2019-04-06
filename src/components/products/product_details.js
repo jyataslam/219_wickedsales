303c4b76c3d4927b862564c9a1dde61ef583460f
Jason <jasonyata@gmail.com>
2019-04-05 18:38:47 -0700

    Currently working through rendering individual product details onto dom

--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ProductDetails extends Component {
-    componentDidMount(){
+    state = {
+        details: null
+    }
+
+    async componentDidMount(){
         const {params} = this.props.match;
+
         // Call server to get product details. Need a product ID
+        const response = await axios.get(`/api/getproductdetails.php?productId=${params.product_id}`);
+        console.log(response);
+
+        if (response.data.success){
+            this.setState({
+                details: response.data.productInfo
+            });
+        } else {
+            this.setState({
+                details: false
+            });
+        }
     }
 
     render() {
+        const { details } = this.state;
+        console.log(details)
+
+        if (details === null){
+            return <h1>Loading...</h1>
+        } else if (!details){
+            return <h1 className="center">No Product Found! 😢</h1>
+        }
+
+        const { name, description } = details;
+
         return (
             <div className="product-details">
-                <h1 className="center">[Product Name] Details</h1>
+                <h1 className="center">{name}</h1>
+                <p className="center">{description}</p>
             </div>
         )
     }
--- src/components/products/product_item.js
+++ src/components/products/product_item.js
@@ -3,11 +3,11 @@ import { formatMoney } from '../../helpers';
 
 // able to destructure in parameter so as not to say props.name, props.price, etc
 // able to also destructure the product image array
-export default ({name, price, images: [ productImg = '']}) => {
+export default ({name, price, id, images: [ productImg = ''], detailsFunction}) => {
     return (
-        <li className="collection-item avatar">
+        <li className="collection-item avatar product-item" onClick={() => {detailsFunction(id)}}>
             <img className="circle" src={`/dist/${productImg}`} alt="default image"/>
-            <span className="title">{name}</span>
+            <span className="title large">{name}</span>
             <p>{formatMoney(price)}</p>
         </li>
     );
