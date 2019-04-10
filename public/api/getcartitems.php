dc3e2c34d56a58419b1610650e706305329fe203
Jason <jasonyata@gmail.com>
2019-04-10 13:16:03 -0700

    updated query to include user id

--- public/api/getcartitems.php
+++ public/api/getcartitems.php
@@ -13,6 +13,7 @@ if (empty($_SESSION['cart_id'])){
     throw new Exception('Missing cart id');
 };
 
+$user_id = 1;
 $cart_id = $_SESSION['cart_id'];
 
 $query = "SELECT c.created, c.total_price, ci.quantity, p.id, p.name, p.price, 
@@ -21,7 +22,7 @@ $query = "SELECT c.created, c.total_price, ci.quantity, p.id, p.name, p.price,
         JOIN cart_items as ci 
         ON ci.carts_id = c.id JOIN products AS p 
         ON ci.products_id = p.id 
-        WHERE c.id = 1";
+        WHERE c.id = $cart_id AND c.`users_id` = $user_id";
 
 $cart_data = mysqli_query($conn, $query);
 
